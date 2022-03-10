7e8ce151be7f29457bee7a1dd1140452428efe17
Kihsomray <zerotoil.network@gmail.com>
2022-03-09 16:27:12 -0800

    Initial Commit (0.0.1)

---
+++ src/main/java/net/zerotoil/dev/cyberclans/CyberClans.java
@@ -0,0 +1,17 @@
+package net.zerotoil.dev.cyberclans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CyberClans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
